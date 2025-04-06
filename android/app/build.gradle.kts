e83e24f58f1c8477fd64c70858e661e8f82cbdc3
kenny-17 <kanishkjha04@gmail.com>
2025-04-06 11:58:55 +0530

    fourth commit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,10 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.concert_connect"
+    namespace = "com.example.concert_connect"  // Ensure this is your app's namespace.
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
-
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.concert_connect"
+        applicationId = "com.example.concert_connect" // Ensure this matches the namespace
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
